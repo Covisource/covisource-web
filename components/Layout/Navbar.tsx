c1dd4e09ed17fe43ebb92364f7a6e84424f1c501
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-04 22:40:17 +0530

    implemented barebones version of location rendering on screen

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -11,6 +11,7 @@ import { useHereContext } from "~contexts/HereContext";
 import Input from "~components/Form/Input";
 
 const Navbar = () => {
+  // config
   const session = useSession();
   const user = session[0]?.user;
   const hereToken = useHereContext();
@@ -18,48 +19,71 @@ const Navbar = () => {
   // router
   const router = useRouter();
 
+  // state
+  const [hits, setHits] = useState<object[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleInputChange = debounce(async (e) => {
     const location = e.target.value;
 
     if (location.replace(" ", "").length > 0) {
-      const res = await axios.get(
-        `https://autosuggest.search.hereapi.com/v1/autosuggest?q=${location}&in=countryCode:IND&at=-13.163068,-72.545128`,
-        {
-          headers: {
-            Authorization: "Bearer " + hereToken,
-          },
-        }
-      );
-      console.log(res.data.items[0]);
+      try {
+        setIsLoading(true);
+        setHits([]);
+        const res = await axios.get(
+          `https://autosuggest.search.hereapi.com/v1/autosuggest?q=${location}&in=countryCode:IND&at=-13.163068,-72.545128`,
+          {
+            headers: {
+              Authorization: "Bearer " + hereToken,
+            },
+          }
+        );
+        setIsLoading(false);
+        setHits(res.data.items);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      setHits([]);
     }
   }, 500);
 
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center">
-        <img
-          src="/logo.png"
-          alt="CoviSource"
-          className="hidden lg:block h-10"
-        />
-        <img src="/logo-sm.png" alt="CoviSource" className="lg:hidden h-10" />
-      </div>
-
-      <div className="flex items-center gap-3">
-        <div className="h-12 flex items-center p-3 rounded-3xl bg-gray-200">
-          <div className="flex items-center gap-1 mr-2">
-            <i className="fad fa-map-marker-alt text-purple-500 text-2xl"></i>
-            <Input placeholder="Enter a location" className="w-auto" subClassName="text-gray-700 text-sm font-medium" onKeyDown={handleInputChange} />
-          </div>
-          <i className="fas fa-caret-down text-gray-700"></i>
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img
+            src="/logo.png"
+            alt="CoviSource"
+            className="hidden lg:block h-10"
+          />
+          <img src="/logo-sm.png" alt="CoviSource" className="lg:hidden h-10" />
         </div>
-        <img
-          src={user ? user.image : ""}
-          alt="Profile Photo"
-          className="w-12 h-12 rounded-full object-contain"
-        />
+
+        <div className="flex items-center gap-3">
+          <div className="h-12 flex items-center p-3 rounded-3xl bg-gray-200">
+            <div className="flex items-center gap-1 mr-2">
+              <i className="fad fa-map-marker-alt text-purple-500 text-2xl"></i>
+              <Input
+                placeholder="Enter a location"
+                className="w-auto"
+                subClassName="text-gray-700 text-sm font-medium"
+                onChange={handleInputChange}
+              />
+            </div>
+            <i className="fas fa-caret-down text-gray-700"></i>
+          </div>
+          <img
+            src={user ? user.image : ""}
+            alt="Profile Photo"
+            className="w-12 h-12 rounded-full object-contain"
+          />
+        </div>
       </div>
-    </div>
+      {isLoading && "Loading"}
+      {hits.length < 1 && !isLoading ? "No Results Found..." : ""}
+      {hits.length > 1 ? hits.map((hit) => hit.title) : ""}
+    </>
   );
 };
 
