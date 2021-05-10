837938f8d8fb4c36c7c3103db12e98d95cbf5c31
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-11 01:46:07 +0530

    completed changing word

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,20 +9,21 @@ export default function Home() {
   return (
     <Layout page="home">
       <div className="flex flex-col justify-center h-full lg:w-3/5">
-        <h1 className="ct-font-mont font-bold text-4xl lg:text-5xl xl:text-6xl mb-4">
-          Find
-          <TextLoop springConfig={{ stiffness: 180, damping: 8 }}>
+        <div className=" mb-4">
+          <span className="ct-font-mont font-bold text-4xl lg:text-5xl xl:text-6xl">
+            Find{" "}
+          </span>
+          <TextLoop>
             {changingWords.map((word, index) => (
-              <span key={index} className="ct-text-grad ct-font-mont font-bold">
+              <span className="ct-text-grad ct-font-mont font-bold text-4xl lg:text-5xl xl:text-6xl">
                 {word}
               </span>
             ))}
-          </TextLoop>
-          <br />
-          From Anywhere In
-          <br />
-          India.
-        </h1>
+          </TextLoop>{" "}
+          <span className="ct-font-mont font-bold text-4xl lg:text-5xl xl:text-6xl">
+            <br /> From Anywhere In <br /> India.
+          </span>
+        </div>
         <p className="ct-font-mont ct-font-muted font-semibold text-md leading-5 mb-8">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque placeat
           nemo animi, numquam nulla
