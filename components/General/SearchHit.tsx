c10511848175beaca66b746884b5ba586b3e602d
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-13 08:41:51 +0530

    added ability for the location to be saved in cookies if user is not logged in

--- components/General/SearchHit.tsx
+++ components/General/SearchHit.tsx
@@ -1,5 +1,9 @@
-import { useSession } from "next-auth/client";
+// dependencies
 import React from "react";
+import cookieCutter from "cookie-cutter";
+
+// session imports
+import { useSession } from "next-auth/client";
 import SessionSchema from "schema/SessionSchema";
 
 interface Props {
@@ -14,23 +18,35 @@ const Hit = (props: Props) => {
   const user: SessionSchema = useSession()[0] as any;
 
   const handleHitClick = async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/user/setUserLocation`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `token ${user?.jwt}`,
-        },
-        body: JSON.stringify({
-          coordinates: {
-            long: props.coordinates[0],
-            lat: props.coordinates[1],
-          },
-        }),
+    // if the user is authenticated, make an api request to update their coordinates, else store them in a cookie
+    if (user) {
+      try {
+        await fetch(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/user/setUserLocation`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `token ${user?.jwt}`,
+            },
+            body: JSON.stringify({
+              coordinates: {
+                long: props.coordinates[0],
+                lat: props.coordinates[1],
+              },
+            }),
+          }
+        );
+      } catch (err) {
+        console.log(err);
       }
-    );
+    } else {
+      cookieCutter.set("coviUserLocationLong", props.coordinates[0]);
+      cookieCutter.set("coviUserLocationLat", props.coordinates[1]);
+      cookieCutter.set("coviUserLocationDisplay", props.title);
+    }
 
+    // set the input value to the title of what they select and then hide the popup
     props.setInputValue(props.title);
     props.hidePopup();
   };
