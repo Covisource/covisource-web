583cb3a4cc810de5817c41aa7d330a9cb61ce4e9
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-21 17:38:22 +0530

    persisting extra parameter data as well

--- localComponents/NewResourceModal/Position3.tsx
+++ localComponents/NewResourceModal/Position3.tsx
@@ -20,12 +20,25 @@ const Position3 = ({ formData, setFormData }) => {
         />
 
         {formData.extraParameters.map((param) => {
+          const indexInFormData = formData.extraParameters.findIndex(
+            (formParam) => formParam.name === param.name
+          );
+
           return (
             <Input
               type={param.type}
               placeholder={param.name}
               subClassName="bg-gray-100"
               prepend={<i className={`${param.icon}`}></i>}
+              value={formData.extraParameters[indexInFormData].value}
+              onChange={(e) => {
+                const newExtraParameters = [...formData.extraParameters];
+                newExtraParameters[indexInFormData].value = e.target.value;
+                setFormData((cur) => ({
+                  ...cur,
+                  extraParameters: newExtraParameters,
+                }));
+              }}
             />
           );
         })}
