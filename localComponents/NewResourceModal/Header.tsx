a29a0a93b45a6e79bc1729de1afd4f9e8190aeec
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-25 02:00:18 +0530

    enhance header

--- localComponents/NewResourceModal/Header.tsx
+++ localComponents/NewResourceModal/Header.tsx
@@ -1,42 +1,41 @@
 import React from "react";
 
-const Header = ({ position, setPosition }) => {
+const Header = ({ position, setPosition, setIsOpen }) => {
   return (
     <div className="flex items-center justify-between border-b border-gray-300 pb-3">
-      <div>
-        <h3 className="text-xl ct-font-mont font-bold">Upload A Resource</h3>
+      <div className="w-full">
+        <div className="flex items-center justify-between w-full">
+          <h3 className="text-xl ct-font-mont font-bold">Upload A Resource</h3>
+          <i className="fas fa-times cursor-pointer" onClick={() => setIsOpen(false)}></i>
+        </div>
         <div className="flex flex-row space-x-4 pt-4 pb-4">
           <PositionButton
             pos={1}
             currentPos={position}
             setCurrentPos={setPosition}
           />
-          <PositionText currentPos={position} pos={1} text="Step 1 - Basics" />
+          <PositionText currentPos={position} pos={1} text="Basics" />
 
           <PositionButton
             pos={2}
             currentPos={position}
             setCurrentPos={setPosition}
           />
-          <PositionText
-            currentPos={position}
-            pos={2}
-            text="Step 2 - Location"
-          />
+          <PositionText currentPos={position} pos={2} text="Location" />
 
           <PositionButton
             pos={3}
             currentPos={position}
             setCurrentPos={setPosition}
           />
-          <PositionText currentPos={position} pos={3} text="Step 3 - Contact" />
+          <PositionText currentPos={position} pos={3} text="Contact" />
 
           <PositionButton
             pos={4}
             currentPos={position}
             setCurrentPos={setPosition}
           />
-          <PositionText currentPos={position} pos={4} text="Step 4 - Extra" />
+          <PositionText currentPos={position} pos={4} text="Extra" />
         </div>
       </div>
     </div>
@@ -46,7 +45,7 @@ const Header = ({ position, setPosition }) => {
 const PositionButton = ({ pos, currentPos, setCurrentPos }) => {
   return (
     <button
-      className={`w-10 h-10 rounded-full ${
+      className={`w-10 h-10 rounded-full focus:outline-none focus:ring-1 focus:ring-gray-900 ${
         currentPos > pos ? "far fa-check" : ""
       }`}
       style={{
@@ -70,9 +69,10 @@ const PositionText = ({ pos, currentPos, text }) => {
   return (
     <div className="flex flex-row">
       <span
-        className={`text-sm ct-text-muted self-center pr-4 ${
+        className={`text-md font-bold ct-text-muted self-center pr-4 ${
           currentPos !== pos && "hidden"
         }`}
+        style={{ color: currentPos === pos && "#4563CB" }}
       >
         {text}
       </span>
@@ -82,9 +82,9 @@ const PositionText = ({ pos, currentPos, text }) => {
           backgroundColor:
             currentPos < pos
               ? "#dcdcdc"
-              : currentPos === pos
-              ? "#4563CB"
-              : "#21C174",
+              : currentPos !== pos
+              ? "#21C174"
+              : "#dcdcdc",
         }}
       ></div>
     </div>
