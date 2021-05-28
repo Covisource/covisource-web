60c753e29b4bb848cbc4ed74e7d6da54ee9bd907
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-29 02:07:06 +0530

    add: description and extra information

--- pages/resource/[resourceId].tsx
+++ pages/resource/[resourceId].tsx
@@ -43,7 +43,7 @@ export const getStaticProps = async ({ params }) => {
 const index = ({ resource }) => {
   return (
     <Layout page="resources">
-      <div className="container mx-auto px-7 mt-5 overflow-hidden">
+      <div className="container mx-auto px-10 mt-5 overflow-hidden">
         <Link href="/">
           <a className="flex items-center gap-2 text-md ct-text-muted font-semibold">
             <i className="far fas fa-arrow-left"></i>
@@ -62,25 +62,49 @@ const index = ({ resource }) => {
               <span className="ct-text-accent font-extrabold">2x</span> Oxygen
               Cylinders Available In Greater Mumbai Area
             </h1>
-            <div className="mt-auto flex justify-between items-baseline">
+            <div className="flex justify-between items-baseline mt-auto">
               <div className="flex items-center gap-4">
-                <span className="flex items-center gap-2 font-bold text-xs">
-                  <i className="fas fa-map-marker-alt"></i>
-                  Udaipur, Rajasthan
-                </span>
-                <span className="flex items-center gap-2 font-bold text-xs">
-                  <i className="fas fa-info-circle"></i>
-                  5l canisters
-                </span>
-                <span className="flex items-center gap-2 font-bold text-xs">
-                  <i className="fas fa-phone-alt"></i>
-                  +91 22 1234 5678
-                </span>
+                <div className="flex items-center gap-4">
+                  <span className="flex items-center gap-2 font-bold text-xs">
+                    <i className="fas fa-map-marker-alt"></i>
+                    Udaipur, Rajasthan
+                  </span>
+                  <span className="flex items-center gap-2 font-bold text-xs">
+                    <i className="fas fa-phone-alt"></i>
+                    +91 22 1234 5678
+                  </span>
+                </div>
+
+                <div className="hidden md:flex">
+                  <span className="flex items-center gap-2 font-bold text-xs">
+                    <i className="fas fa-info-circle"></i>
+                    5l canisters
+                  </span>
+                </div>
               </div>
               <h1 className="font-bold text-3xl">
                 <i className="fas fa-rupee-sign mr-1"></i>
                 999
               </h1>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6 mt-5">
+            <div className="md:hidden flex flex-col ct-bg-muted rounded-lg p-7">
+              <span className="flex items-center gap-2 font-bold text-xs">
+                <i className="fas fa-info-circle"></i>
+                5l canisters
+              </span>
+            </div>
+            <div>
+              <h2 className="text-md uppercase" style={{ fontWeight: 800 }}>
+                # Description
+              </h2>
+              <p className="text-lg leading-5 font-bold ct-text-muted mt-2">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Excepturi amet pariatur a fuga, autem soluta voluptate tempora
+                quisquam natus dolorem tempore! Saepe, odit incidunt animi
+                nostrum facere commodi cupiditate soluta.
+              </p>
             </div>
           </div>
         </div>
